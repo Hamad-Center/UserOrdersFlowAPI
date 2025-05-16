157bd644ceeba4d4229542495d30447bee8ff09f
ahmedabougabal <gabal@hamad.center>
2025-05-16 09:58:49 +0300

    feat: implements skeleton of routes in the users controllers defined in the users app, all tested using postman and thunderclient and works as expected

---
+++ src/users/users.controller.ts
@@ -0,0 +1,36 @@
+import {Controller, Get, Param, Post, Patch, Delete, Body, Query} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+    /*
+    * GET /users
+    * GET /users/:id
+    * POST /users
+    * PATCH /users/:id
+    * DELETE /users/:id
+    * */
+    @Get()
+    findAll(@Query('role') role?:'INTERN' | 'ENGINEER' |'ADMIN'){
+        return []
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string){
+        return {id}
+    }
+
+    @Post()
+    create(@Body() user:{}){
+        return user
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() userUpdate:{} ){
+        return {id, userUpdate }
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string){
+        return {id}
+    }
+}
