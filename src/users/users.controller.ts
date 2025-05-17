9930de8a75abe5aa94cfb7b46d41ed8d9860fbbe
ahmedabougabal <gabal@hamad.center>
2025-05-17 22:40:33 +0300

    feat: implements dependency injections of methods in services into controllers accroding to documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
-import {Controller, Get, Param, Post, Patch, Delete, Body, Query} from '@nestjs/common';
+import { Controller, Get, Param, Post, Patch, Delete, Body, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
@@ -9,28 +10,30 @@ export class UsersController {
     * PATCH /users/:id
     * DELETE /users/:id
     * */
+    constructor(private usersService: UsersService) { }
+
     @Get()
-    findAll(@Query('role') role?:'INTERN' | 'ENGINEER' |'ADMIN'){
-        return []
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        return this.usersService.findAll(role);
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string){
-        return {id}
+    findOne(@Param('id') id: string) {
+        return this.usersService.findOne(parseInt(id));
     }
 
     @Post()
-    create(@Body() user:{}){
-        return user
+    create(@Body() user: { name: string, email: string, role: 'INTERN' | 'ADMIN' | 'ENGINEER' }) {
+        return this.usersService.create(user);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() userUpdate:{} ){
-        return {id, userUpdate }
+    update(@Param('id') id: string, @Body() userUpdate: { name?: string, email?: string, role?: 'INTERN' | 'ADMIN' | 'ENGINEER' }) {
+        return this.usersService.update(+id, userUpdate);
     }
 
     @Delete(':id')
-    delete(@Param('id') id: string){
-        return {id}
+    delete(@Param('id') id: string) {
+        return this.usersService.delete(+id);
     }
 }
