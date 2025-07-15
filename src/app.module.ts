02ee2ba306f95b26bf25a73ecbd260a6a63c4f89
ahmedabougabal <gabal@hamad.center>
2025-07-15 17:16:05 +0300

    ref: adds modified directories paths for the new tree structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
+import { ClassesModule } from './modules/classes/classes.module';
 
 @Module({
-  imports: [UsersModule], // added after i have added the users module using the nest cli cmd : nest g module users
+  imports: [
+    UsersModule,
+    ClassesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
