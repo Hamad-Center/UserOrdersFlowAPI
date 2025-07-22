0a32af922f5eff7951abd622c54d086cfd779a77
ahmedabougabal <gabal@hamad.center>
2025-07-22 17:33:51 +0300

    feat: adds classEventController to classes.module.ts

--- src/modules/classes/classes.module.ts
+++ src/modules/classes/classes.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ClassesController } from './controllers/classes.controller';
 import { ClassesService } from './services/classes.service';
-import { BatchAssignUsersDto } from './dto/batch-assign-users.dto';
 import { BatchProcessorService } from './services/batch-processor.service';
+import { ClassEventsController } from './controllers/class-events.controller';
 
 @Module({
-    controllers: [ClassesController],
+    controllers: [ClassesController,
+        ClassEventsController,
+    ],
     providers: [ClassesService, BatchProcessorService],
     exports: [ClassesService], // Export for use in other modules
 })
